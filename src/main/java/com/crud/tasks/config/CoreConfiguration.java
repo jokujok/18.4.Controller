ee83af6bf670f0b2b928e757df97377ae305f88a
Artur Cieslinski <jokujok1492@gmail.com>
2022-09-17 14:03:59 +0200

    AddingGroupOpenApi

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.config;
 
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,5 +13,25 @@ public class CoreConfiguration {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+/*
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        String pathsToMatch[] = {"/v1/tasks"};
+        return GroupedOpenApi.builder()
+                .group("tasks")
+                .pathsToMatch(pathsToMatch)
+                .build();
+    }
+*/
 
+    @Bean
+    public GroupedOpenApi groupOpenApi() {
+        String paths[] = {"/v1/tasks"};
+        String packagesToscan[] = {"com.crud.tasks.controller"};
+        return GroupedOpenApi.builder()
+                .group("")
+                .pathsToMatch(paths)
+                .packagesToScan(packagesToscan)
+                .build();
+    }
 }
